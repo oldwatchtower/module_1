bc4e1e0d92815e6f353987fc5633489d6c90c6bf
dungeon priest <oppressivenight@gmail.com>
2024-02-15 16:15:58 +0200

    решил первую задачу модуль 1, тема 3

---
+++ lesson_04/script.js
@@ -0,0 +1,50 @@
+'use strict'
+
+{
+  const productName = 'Banana',
+      productCategory = 'Fruit';
+
+let productAmount = 20,
+    productPrice = 70;
+
+console.log(productName);
+
+console.log(productAmount * productPrice);
+}
+
+{
+  const item = 'phone',
+        category = 'technique';
+  
+  let count = 10,
+      price = '15000';
+}
+
+/* {
+  const resultProduct = prompt('Наименование товара');
+  console.log(typeof resultProduct);
+
+  const resultCount = +prompt('Количество товара');
+  console.log(typeof resultCount);
+
+  const resultCategory = prompt('Категория товара');
+  console.log(typeof resultCategory);
+
+  const resultPrice = +prompt('Цена товара');
+  console.log(typeof resultPrice);
+} */
+
+
+{
+  const resultProduct = prompt('Наименование товара'),
+        resultCount = +prompt('Количество товара'),
+        resultCategory = prompt('Категория товара'),
+        resultPrice = +prompt('Цена товара');
+  
+  console.log(typeof resultProduct);
+  console.log(typeof resultCount);
+  console.log(typeof resultCategory);
+  console.log(typeof resultPrice);
+
+  console.log(`На складе ${resultCount} единицы товара ${resultProduct} на сумму ${resultCount * resultPrice}`);
+}
