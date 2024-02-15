f362422ae1278c5b7cd431f8d208f0ff4f585e9c
dungeon priest <oppressivenight@gmail.com>
2024-02-15 11:51:40 +0200

    Инициализируйте локальный репозиторий в корневой папке первого модуля

---
+++ lesson_02/script.js
@@ -0,0 +1,9 @@
+const productName = 'Banana',
+      productCategory = 'Fruit';
+
+let productAmount = 20,
+    productPrice = 70;
+
+console.log(productName);
+
+console.log(productAmount * productPrice);
