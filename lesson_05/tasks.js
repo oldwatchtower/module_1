e3cb2b91c79b19a97f72033ca40439bf1b2f0904
dungeon priest <oppressivenight@gmail.com>
2024-02-18 10:10:21 +0200

    Выполнена вторая задача

---
+++ lesson_05/tasks.js
@@ -0,0 +1,8 @@
+const rain = Math.round(Math.random());
+console.log(rain);
+
+if (rain === 1) {
+  console.log('Пошел дождь. Возьмите зонт!');
+} else if (rain === 0) {
+  console.log('Дождя нет!');
+}
