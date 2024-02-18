8042ace182960d0d0e17e13cb198c50ce9486c83
dungeon priest <oppressivenight@gmail.com>
2024-02-18 10:38:06 +0200

    Выполнил основные задания модуль 1 тема 4

--- lesson_05/tasks.js
+++ lesson_05/tasks.js
@@ -1,8 +1,34 @@
-const rain = Math.round(Math.random());
+/* Задача два */
+/* const rain = Math.round(Math.random());
 console.log(rain);
 
 if (rain === 1) {
   console.log('Пошел дождь. Возьмите зонт!');
 } else if (rain === 0) {
   console.log('Дождя нет!');
+} */
+
+
+/* Задача три */
+
+/* const mathScores = +prompt('Введите кол-во баллов по математике'),
+      langScores = +prompt('Введите кол-во баллов по русскому языку'),
+      informScores = +prompt('Введите кол-во баллов по информатике');
+
+const totalScores = mathScores + langScores + informScores;
+
+if (totalScores >= 265) {
+  console.log('Поздравляю, вы поступили на бюджет!')
+} */
+
+
+/* Задача четыре */
+
+const howMuchMoneyDoYouNeed = prompt('Сколько денег вы хотите снять?');
+
+if (howMuchMoneyDoYouNeed % 100 === 0) {
+  console.log('Идет выдача денег');
+} else {
+  console.log('Введите сумму, кратную 100 рублям');
 }
+
