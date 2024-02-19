53354e21b4d39dbfb09f044e73ddfc879295ff37
dungeon priest <oppressivenight@gmail.com>
2024-02-19 10:47:40 +0200

    Выполнил основное задание модуль 1 тема 5

--- lesson_06/task04.js
+++ lesson_06/task04.js
@@ -1,23 +1,27 @@
-const calculate = (total, amount, promo) => {
-  let totalPrice = total;
-  let totalSale;
-  if (amount > 10) {
-    totalSale = totalPrice * 0.03;
-    console.log(totalSale);
-  }
-  if (total > 30000) {
-    totalSale += totalPrice * 0.15;
-    console.log(totalSale);
+const calculate = (price, amountProd, promo) => {
+  let totalPrice = price;
+  let sumSale;
+  if (amountProd > 10) {
+    totalPrice *= 0.97;
+    console.log(totalPrice);
   }
 
-  if (promo === 'METHED') {
-    totalSale += totalPrice * 0.1
-    console.log(totalSale);
+  if (totalPrice > 30000) {
+    sumSale = (totalPrice - 30000) * 0.15; 
+    totalPrice -= sumSale;
+    console.log(totalPrice);
   }
-  return totalPrice - totalSale;
-  // if (promo === 'G3H2Z1' && totalPrice > 2000) {
-  //   totalSale = totalPrice - 500; 
-  // }
+  
+  if (promo === 'METHED') {
+    totalPrice *= 0.9;
+    console.log(totalPrice);
+  }
+
+  if (promo === 'G3H2Z1' && totalPrice > 2000) {
+    totalPrice -= 500;
+    console.log(totalPrice);
+  }
+  return totalPrice.toFixed(2);
 }
 
 console.log(calculate(32000, 11, 'METHED'));
